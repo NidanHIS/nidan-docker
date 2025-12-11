04532dec6d58f869e926ed0ad9421a74211cd0d2
Dipak Thapa <dipakthapaofficial@gmail.com>
2025-12-11 22:03:31 +0545

    Adds confis for nidan

--- debug_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Starting debug script..." > /Users/dipakthapa/codehome/repos/nidanhis/nidan-docker/debug_log.txt
-docker exec nidan-openmrs-backend ls -R /openmrs/distribution >> /Users/dipakthapa/codehome/repos/nidanhis/nidan-docker/debug_log.txt 2>&1
-echo "Done." >> /Users/dipakthapa/codehome/repos/nidanhis/nidan-docker/debug_log.txt
